ed9bdc0b25dd9cd37dbfa69cc3264e99eb68e43d
Chris Oei <chris.oei@gmail.com>
2022-10-12 14:43:03 -0700

    Clicking on Joplin code now copies to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import './App.css';
 
 const dxlx = (x: string): JSX.Element => {
     let y = parseFloat(x);
+    let j1 = document.getElementById("j1");
+    if (j1) {
+        j1.style.backgroundColor = "black";
+    }
     return (<span>{y.toExponential(3)}, or 10<sup>{Math.log10(y).toFixed(2)}</sup></span>);
+}
+
+const joplin_clipboard = (y: number): void => {
+    let s = y.toExponential(3) + ", or 10<sup><b>" + Math.log10(y).toFixed(2) + "</b></sup>"
+    navigator.clipboard.writeText(s);
+    let j1 = document.getElementById("j1");
+    if (j1) {
+        j1.style.backgroundColor = "green";
+    }
 }
 
 const joplin_dxlx = (x: string): JSX.Element => {
     let y = parseFloat(x);
-    return (<span>{y.toExponential(3)}, or 10&lt;sup&gt;&lt;b&gt;{Math.log10(y).toFixed(2)}&lt;/b&gt;&lt;/sup&gt;</span>);
+    return (<span id="j1" onClick={() => joplin_clipboard(y)}>{y.toExponential(3)}, or 10&lt;sup&gt;&lt;b&gt;{Math.log10(y).toFixed(2)}&lt;/b&gt;&lt;/sup&gt;</span>);
 }
 
 const App: React.FC = () => {
