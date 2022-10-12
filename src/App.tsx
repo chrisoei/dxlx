c7ad48c4227e44f307ba5de781de72d0e8f5e9cc
Chris Oei <chris.oei@gmail.com>
2022-10-12 14:13:20 -0700

    Tweak formatting and display.

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,16 @@ const dxlx = (x: string): JSX.Element => {
 
 const joplin_dxlx = (x: string): JSX.Element => {
     let y = parseFloat(x);
-    return (<span className="joplin">{y.toExponential(3)}, or 10&lt;sup&gt;&lt;b&gt;{Math.log10(y).toFixed(2)}&lt;/b&gt;&lt;/sup&gt;</span>);
+    return (<span>{y.toExponential(3)}, or 10&lt;sup&gt;&lt;b&gt;{Math.log10(y).toFixed(2)}&lt;/b&gt;&lt;/sup&gt;</span>);
 }
 
 const App: React.FC = () => {
-    const [x, setX] = useState("0.0");
+    const [x, setX] = useState("1.0");
     return (
-        <div>
-            <div>Input value: <input id="x" type="text" onChange={(e: React.FormEvent<HTMLInputElement>) => setX(e.currentTarget.value) }></input></div>
-            <div>Display value: {dxlx(x)}</div>
-            <div>Joplin: {joplin_dxlx(x)}</div>
+        <div className="main">
+            <div className="input">Input value: <input id="x" type="text" value="1.0" onChange={(e: React.FormEvent<HTMLInputElement>) => setX(e.currentTarget.value) }></input></div>
+            <div className="display">Display value: {dxlx(x)}</div>
+            <div className="joplin">Joplin: {joplin_dxlx(x)}</div>
         </div>
     )
 }
