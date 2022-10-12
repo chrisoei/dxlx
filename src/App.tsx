279fcf5b8322b165f5c16b6eee0b2d15bb63a240
Chris Oei <chris.oei@gmail.com>
2022-10-12 10:22:57 -0700

    Implemented basic functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dxlx = (x: string): JSX.Element => {
+    let y = parseFloat(x);
+    return (<span>{y.toExponential(3)}, or 10<sup>{Math.log10(y).toFixed(2)}</sup></span>);
 }
 
-export default App;
+const joplin_dxlx = (x: string): JSX.Element => {
+    let y = parseFloat(x);
+    return (<span className="joplin">{y.toExponential(3)}, or 10&lt;sup&gt;&lt;b&gt;{Math.log10(y).toFixed(2)}&lt;/b&gt;&lt;/sup&gt;</span>);
+}
+
+const App: React.FC = () => {
+    const [x, setX] = useState("0.0");
+    return (
+        <div>
+            <div>Input value: <input id="x" type="text" onChange={(e: React.FormEvent<HTMLInputElement>) => setX(e.currentTarget.value) }></input></div>
+            <div>Display value: {dxlx(x)}</div>
+            <div>Joplin: {joplin_dxlx(x)}</div>
+        </div>
+    )
+}
+
+export default App
